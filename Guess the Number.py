e0280daba2fe18c13715436f4b204de487c516f0
JustinHendricks <47921340+JustinHendricks@users.noreply.github.com>
2019-10-03 12:57:21 -0400

    Add files via upload

---
+++ Guess the Number.py
@@ -0,0 +1,27 @@
+import random
+
+i = 0 
+
+number = random.randint(0, 100)
+
+guess = int(input("Guess the number "))
+distances = []
+distances.append(abs(int(number - guess)))
+
+while guess != number:
+    if len(distances) > 1:
+        if distances[i] < distances[i - 1]:
+            print("You are getting warmer")
+        elif distances[i] > distances[i - 1]:
+            print("You are getting colder")
+    if distances[i] < 10:
+        print("You are red hot")
+    elif distances[i] >= 10 and distances[i] <= 20:
+        print("You are warm")
+    elif distances[i] > 20:
+        print("You are cold")
+    guess = int(input("Guess the number "))
+    distances.append(abs(int(number - guess)))
+    i += 1
+
+print("You are correct! You got it after " + str(len(distances)) , "guesses.")
